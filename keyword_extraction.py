a95762f592b3a5d657511ec617dff419aa53c185
yash <shyash222@gmail.com>
2021-06-27 20:15:53 +0530

    remove bug

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -1,9 +1,7 @@
 from nltk import tokenize
 from operator import itemgetter
 import math
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+import nltk 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
 stop_words = set(stopwords.words('english'))
